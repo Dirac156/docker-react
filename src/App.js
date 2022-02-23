8f15e251ffb3a5f1bc4d4936cb16e50d4ca44a5f
dirac156 <dmurairimukongya@gmail.com>
2022-02-23 13:34:12 +0200

    added features branch

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello there! heyyyy.
-          I hacked react js:))):()))))) 
+          My name is Dirac
         </p>
         <a
           className="App-link"
